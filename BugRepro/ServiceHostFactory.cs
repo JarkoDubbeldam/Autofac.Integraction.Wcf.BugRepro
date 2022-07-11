e2c66dda4449e3cab2601e55a0d2e04bf1cd1f3e
jdubbeldam <jdubbeldam@independer.nl>
2022-07-11 10:19:36 +0200

    Add project files.

---
+++ BugRepro/ServiceHostFactory.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using Autofac.Integration.Wcf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+
+namespace BugRepro {
+  public class ServiceHostFactory : AutofacWebServiceHostFactory {
+
+    public override ServiceHostBase CreateServiceHost(string constructorString, Uri[] baseAddresses) {
+      Container = new ContainerBuilder().Build();
+
+      return base.CreateServiceHost(constructorString, baseAddresses);
+    }
+  }
+}
